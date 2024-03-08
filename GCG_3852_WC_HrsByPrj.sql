c06fb5b5ad3474efca04908c7af45b517384ff62
Service Web <sw@gssmail.com>
2024-03-08 17:18:27 -0600

    Commit changes in C:\Websites\Git\emarlow\3852\GCG_3852_WC_HrsByPrj.sql

---
+++ GCG_3852_WC_HrsByPrj.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_3852_WC_HrsByPrj"(
+ "Trmnl" CHAR(3),
+ "Project" CHAR(7),
+ "Phase" CHAR(4),
+ "Descr" CHAR(30),
+ "Machine" CHAR(4),
+ "Hours_Worked" NUMERIC(12,4),
+ "Amt_Labor" NUMERIC(12,4),
+ "Amt_Overhead" NUMERIC(12,4));
